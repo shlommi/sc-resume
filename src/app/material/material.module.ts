41a3070e82dff61e01273da4ec4ac385ee3cd98b
shlommi <shlommi@gmail.com>
2022-10-22 23:33:04 +0300

    added submit button

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+
 
 const materialModules: any[] = [
   MatIconModule,
-  MatInputModule
+  MatInputModule,
+  MatButtonModule
 ]
 
 @NgModule({
